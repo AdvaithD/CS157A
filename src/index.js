25afaa2f51b6c6a4dbbffb66ee5f74a4883d2506
Carbon <advaith@chainist.com>
2020-09-10 16:57:13 -0700

    created a basic server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import 'dotenv/config';
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import log from 'npmlog'
 
-console.log(process.env.SOME_ENV_VARIABLE);
+const app = express()
+
+let corsOptions = {
+    origin: 'http://localhost:8000'
+}
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json({ 'message': 'Welcome to adafruit, we love arduino' })
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    log.info('INIT', `Server is now running on port ${PORT}`)
+})
